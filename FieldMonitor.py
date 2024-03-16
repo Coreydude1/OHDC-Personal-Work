1b8045ee1e912175fb7ee0f79644197f25b69fbc
Corey <c.gardner@rgu.ac.uk>
2024-03-16 18:41:57 +0000

    made movement easier

--- FieldMonitor.py
+++ FieldMonitor.py
@@ -6,12 +6,36 @@ class Animal:
 
     def __init__(self) -> None:
         self.id = len(animals)
-        self.currentLocation = [0,0]
-        self.previousLocation = [0,0]
+        self.currentLocation = Point(0,0)
+        self.previousLocation = Point(0,0)
         animals.append(self)
 
     def __str__(self) -> str:
-        return (f"Id : {self.id} \nCurrent Location : {self.currentLocation} \nPrevious Location : {self.previousLocation}")
+        return (f"Id : {self.id} \nCurrent Location : ({self.currentLocation}) \nPrevious Location : ({self.previousLocation})")
+    
+    def move(self):
+        animal1.previousLocation.lat = animal1.currentLocation.lat
+        animal1.previousLocation.long = animal1.currentLocation.long
+
+        coords = GpsEmulator.readGpsInput([self.currentLocation.lat, self.currentLocation.long])
+
+        animal1.currentLocation.lat = coords[0]
+        animal1.currentLocation.long = coords[1]
+    
+class Point:
+
+    def __init__(self, lat, long) -> None:
+        self.lat = lat
+        self.long = long
+
+    def __str__(self) -> str:
+        return (f"Lat : {self.lat}, Long : {self.long}")
+
+class Line:
+
+    def __init__(self, points) -> None:
+        self.points = points
+    
 
 animals = []
 
@@ -23,10 +47,9 @@ for animal in animals:
 
 try:
     while True:
-        animal1.previousLocation = animal1.currentLocation
-        animal1.currentLocation = GpsEmulator.readGpsInput(animal1.currentLocation)
+        animal1.move()
         print(animal1)
-        time.sleep(0.5)
+        time.sleep(1)
 
 except KeyboardInterrupt:
     exit()
